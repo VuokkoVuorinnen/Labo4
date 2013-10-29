a075b7d48d39ade801879599de9a87f32e345544
VuokkoVuorinnen <vuokko.vuorinnen@gmail.com>
2013-10-29 10:56:27 +0100

    MySQL config updated

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,8 @@
 node default {
+	
 	package { [ 'httpd', 'php', 'mysql', 'mysql-server', 'php-mysql' ] : 
 		ensure => installed,
+		before => Service['httpd', 'mysqld']
 	}
 
 	service { [ 'httpd', 'mysqld' ] :
