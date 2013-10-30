a994dd4d5ab0f17f9607c6c92ed08302afc0e048
VuokkoVuorinnen <vuokko.vuorinnen@gmail.com>
2013-10-30 13:54:05 +0100

    Site.pp modified for automated sql insertion

--- manifests/site.pp
+++ manifests/site.pp
@@ -14,11 +14,19 @@ node default {
 	
 	class { 'apache::mod::php': }
 
+	file {
+		"Make sure the wordpress.sql file exists":
+		ensure => file,
+		path   => '/var/www/html/wordpress.sql',
+  	}->
+
 	mysql::db { 'wordpress':
-		user     => 'vagrant',
-		password => 'vagrant',
-		host     => 'localhost',
-		grant    => ['ALL'],
+		user		=> 'vagrant',
+		password	=> 'vagrant',
+		host		=> 'localhost',
+		grant		=> ['ALL'],
+		sql 		=> '/var/www/html/wordpress.sql',
+		enforce_sql => true,
 	}
 
 }
