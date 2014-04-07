e5771a261b197e1b625dc786f5ef74639fc46fd0
Patrick Van Brussel <patrick@inuits.eu>
2014-04-07 16:10:57 +0200

    Older revision of concat, cuz the new one is crap
    Signed-off-by: Patrick Van Brussel <patrick@inuits.eu>

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,31 +1,33 @@
 node default {	
-	
-	class { 
-		'mysql::server':;
-		'mysql::bindings':php_enable => true;
-		'apache':default_ssl_vhost => true;
-		'apache::mod::ssl':;
-		'apache::mod::php':;
-	}
 
-	file { 'Make sure the wordpress.sql file exists':
-		ensure => file,
-		path   => '/var/www/html/wordpress.sql',
-  	}->
+  class { 
+    'mysql::server':;
+    'mysql::bindings':php_enable => true;
+    'apache':default_ssl_vhost   => true;
+    'apache::mod::ssl':;
+    'apache::mod::php':;
+  }
 
-	mysql::db { 'wordpress':
-		user        => 'vagrant',
-		password    => 'vagrant',
-		host        => 'localhost',
-		grant       => ['ALL'],
-		sql         => '/var/www/html/wordpress.sql',
-		enforce_sql => true,
-	}
+  file { 'wordpress.sql':
+    ensure  => file,
+    path    => '/var/www/html/wordpress.sql',
+  }
+  
+  mysql::db { 'wordpress':
+    user        => 'vagrant',
+    password    => 'vagrant',
+    host        => 'localhost',
+    grant       => ['ALL'],
+    sql         => '/var/www/html/wordpress.sql',
+    enforce_sql => true,
+    require     => File['wordpress.sql'],   
+  }
 
-	exec { 'Bats testing':
-		require => Service['httpd'],
-		command => 'bats /home/vagrant/tests/lamp.bats',
-		path    => '/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/vagrant/bin',
-	}
+  exec { 'Bats testing':
+    require   => Service['httpd'],
+    command   => 'bats /home/vagrant/tests/lamp.bats',
+    path      => '/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/vagrant/bin',
+    logoutput => true,
+  }
 
 }
